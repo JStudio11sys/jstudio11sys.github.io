9978de1c51f39435d7d902ec11ae016f264278ec
JStudio11sys <126428980+JStudio11sys@users.noreply.github.com>
2023-03-21 19:23:40 +0800

    New Repo

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//function scrollFunction() {
+        //let sky = document.getElementById('sky');
+        //let clouds = document.getElementById('clouds');
+        ///let facade = document.getElementById('facade');
+        //let text = document.getElementById('text');
+        //let btn = document.getElementById('btn');
+        //let students = document.getElementById('students');
+
+        //window.addEventListener('scroll', function(){
+            //let value = window.scrollY;
+            //sky.style.top = value * 0.25 + 'px';
+            //clouds.style.top = value * 1 + 'px';
+            //facade.style.top = value * 0 + 'px';
+            //students.style.left = value * -1 + 'px';
+        //})
+//}
+
