52caeb4636bbd976a65e63d02194c7fbd4f2c906
JStudio11sys <126428980+JStudio11sys@users.noreply.github.com>
2023-03-22 10:07:19 +0800

    Updated Mar 22 miniproject 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 function scrollFunction() {
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 80) {
+          document.getElementById("navbar").style.padding = "80px 10px";
+          document.getElementById("logo").style.fontSize = "20px";
+          document.getElementById("logo").style.paddingTop = "15px";
+          document.getElementById("sta_logo").style.maxWidth = "50px";
+        } else {
+          document.getElementById("navbar").style.padding = "90px 10px";
+          document.getElementById("logo").style.fontSize = "35px";
+          document.getElementById("logo").style.paddingTop = "26px";
+          document.getElementById("sta_logo").style.maxWidth = "80px";
+        }
+      
         let sky = document.getElementById('sky');
         let clouds = document.getElementById('clouds');
         let facade = document.getElementById('facade');
@@ -9,7 +21,7 @@ function scrollFunction() {
         window.addEventListener('scroll', function(){
             let value = window.scrollY;
             sky.style.top = value * 0.25 + 'px';
-            clouds.style.top = value * -1 + 'px';
+            clouds.style.top = value * -0.5 + 'px';
             facade.style.top = value * 0 + 'px';
             students.style.left = value * -1 + 'px';
         })
