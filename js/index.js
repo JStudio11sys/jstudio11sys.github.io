f986665d933f3034f0d11cea52ed65a084ee934f
JStudio11sys <126428980+JStudio11sys@users.noreply.github.com>
2023-04-05 01:31:51 +0800

    fix responsiveness

--- js/index.js
+++ js/index.js
@@ -16,3 +16,20 @@ function scrollFunction() {
       document.getElementById("sta_logo").style.maxWidth = "80px";
     }
 }
+
+
+document.onclick = () => applyCursorRippleEffect(event); 
+
+function applyCursorRippleEffect(e) {
+   const ripple = document.createElement("div");
+
+   ripple.className = "ripple";
+   document.body.appendChild(ripple);
+
+  ripple.style.left = `${e.clientX}px`;
+  ripple.style.top = `${e.clientY}px`; 
+
+   ripple.style.animation = "ripple-effect .4s  linear";
+   ripple.onanimationend = () => document.body.removeChild(ripple);
+
+}
